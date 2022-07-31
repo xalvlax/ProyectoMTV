b21dfc621c1fac6746444a3332268c5cfde3628d
xalvlax <sergiosalvatierra86@gmail.com>
2022-07-31 19:08:49 -0300

    Se creo Admin. Se modifico vista de Modelos en Admin

--- ProyectoCoder/AppCoder/admin.py
+++ ProyectoCoder/AppCoder/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from AppCoder.models import Curso, Entregable, Estudiante, Profesor
 # Register your models here.
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'camada'] # Muestra en tablas
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ['apellido', 'nombre', 'email', 'profesion'] # Muestra en tablas
+    search_fields = ['nombre', 'apellido'] # Buscador por nombre o apellido
+
+admin.site.register(Curso, CursoAdmin)
+
+admin.site.register(Profesor, ProfesorAdmin)
+
+admin.site.register(Estudiante)
+
+admin.site.register(Entregable)
--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -1,3 +1,5 @@
+from codecs import unicode_escape_decode
+from enum import unique
 from django.db import models
 
 # Create your models here.
@@ -5,12 +7,26 @@ class Curso(models.Model):
 
     nombre = models.CharField(max_length=50)
     camada = models.IntegerField()
+    class Meta():
+        ordering = ('nombre', '-camada') # Ordena por curso asc. y por camada desc.
+        unique_together = ('nombre', 'camada') # No permite que se repitan curso y camada
 
+    #def __str__(self):
+    #    return f'{self.nombre} - {self.camada}'
+    # Muestra en la pagina del Admin "apellido, nombre"
+   
 class Estudiante(models.Model):
 
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=40)
     email = models.EmailField()
+    class Meta():
+        ordering = ('apellido', 'nombre') # Ordena por appellido y nombre.
+        unique_together = ('nombre', 'apellido', 'email') # No permite que se repitan.
+
+    def __str__(self) -> str:
+       return f'{self.apellido}, {self.nombre}'
+    # Muestra en la pagina del Admin "apellido, nombre"
 
 class Profesor(models.Model):
 
@@ -18,9 +34,24 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=40)
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
+    class Meta():
+        ordering = ('apellido', 'nombre') # Ordena por apellido y nombre.
+        unique_together = ('apellido', 'nombre', 'email', 'profesion') # No permite que se repitan.
 
+    #def __str__(self) -> str:
+    #    return f'{self.apellido}, {self.nombre} - {self.profesion}'
+    # Muestra en la pagina del Admin "apellido, nombre - profesion"
 class Entregable(models.Model):
 
     nombre = models.CharField(max_length=50)
     fechaDeEntrega = models.DateField()
     entregado = models.BooleanField()
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, default=1)
+    # Clave Foranea. Vincula a otro Modelo. Relacion uno a muchos.
+    # Un estudiante puede entregar varios entregables.
+    # Un entregable puede ser entregado por un estudiante.
+    # En estos casos se crea la clave foranea en el Modelo Secundario.
+
+    def __str__(self) -> str:
+        return f'{self.nombre} - {self.fechaDeEntrega}'
+    # Muestra en la pagina del Admin "nombre - fechaDeEntrega"
